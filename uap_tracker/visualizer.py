012c82d7d657cf1db8d2d8f2e3593a3a12f80209
UAP Paul <uappaul56@gmail.com>
2021-11-18 07:57:56 -0800

    Fix font size in visualizer

--- uap_tracker/visualizer.py
+++ uap_tracker/visualizer.py
@@ -1,10 +1,11 @@
 from uap_tracker import utils
 from uap_tracker.tracker import Tracker
 
+
 class Visualizer():
     def __init__(self):
         self.max_display_dim = 1080
-        self.font_size = 8
+        self.font_size = max(1, self.max_display_dim/250)
         self.font_colour = (50, 170, 50)
 
     def display(self, frame):
@@ -15,6 +16,7 @@ class Visualizer():
 
     def trackers_updated_callback(self, video_tracker):
         frame_output = self.visualise_frame(video_tracker)
+
         self.display(frame_output)
 
     def finish(self, total_trackers_started, total_trackers_finished):
