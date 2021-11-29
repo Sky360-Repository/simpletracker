bded195a954ed95ddf50284d51d557b124806157
Michael G <michael@mwgtechnologies.co.uk>
2021-11-29 17:01:01 +0000

    Included some more volumes as per Dusty's examples

--- docker/run.sh
+++ docker/run.sh
@@ -5,10 +5,12 @@ cp ../settings.mike.toml ../settings.toml
 
 sudo docker run \
       --runtime nvidia \
-      -it --rm \
       --name simpletracker \
+      -it --rm --net=host \
       --security-opt seccomp=unconfined \
-      --net=host \
       -e DISPLAY=$DISPLAY \
+      -v /tmp/.X11-unix/:/tmp/.X11-unix \
+      -v /tmp/argus_socket:/tmp/argus_socket \
+      -v /etc/enctune.conf:/etc/enctune.conf \
       -v /home/$USER/simpletracker:/home/simpletracker \
       nvcr.io/nvidia/l4t-ml:r32.6.1-py3
