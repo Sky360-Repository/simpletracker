b2fb5053f95be812ef6a49e2d0091775fe35d55d
Michael G <michael@mwgtechnologies.co.uk>
2021-11-29 12:23:13 +0000

    Working on the nano setup script

--- nano-setup.sh
+++ nano-setup.sh
@@ -3,15 +3,29 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install gedit
-sudo apt install python3-pip
+#sudo apt install gedit
+#sudo apt install python3-pip
 
-pip3 install dynaconf
+#pip3 install dynaconf
 
-python3 -c 'import cv2; print(cv2.__version__)'
+#python3 -c 'import cv2; print(cv2.__version__)'
 
 cd ~
-mkdir Sky360-Repository
-cd Sky360-Repository
+
 git clone https://github.com/Sky360-Repository/simpletracker.git
-cd simpletracker
+git clone https://github.com/dusty-nv/jetson-inference.git
+
+#cd simpletracker
+
+#sudo docker run \
+#      --runtime nvidia \
+#      -it --rm \
+#      --name simpletracker \
+#      --security-opt seccomp=unconfined \
+#      --net=host \
+#      -e DISPLAY=$DISPLAY \
+#      -v /home/$USER/simpletracker:/app \
+#      nvcr.io/nvidia/l4t-ml:r32.6.1-py3
+
+#sudo docker exec -it nvc /bin/bash
+#python3
