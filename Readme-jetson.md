fdbc9c9ddf849a671eff1974869e961a1009b091
Michael G <michael@mwgtechnologies.co.uk>
2021-12-06 08:14:32 +0000

    Tidy up and also including how to install jtop

--- Readme-jetson.md
+++ Readme-jetson.md
@@ -1,24 +1,32 @@
-###To get simpletracker running on the nano do the following:
+##Installing jtop:
+1. Launch the terminal
+2. Change directory to simpletracker/scripts: ```cd ~/simpletracker/scripts```
+3. Run the jtop installation script: ```./install-jtop.sh``` **NOTE:** The script needs to reboot your Jetson device for the installation to complete
+4. After you have rebooted, you can access jtop by launching the terminal and typing ```jtop``` at the command prompt
+
+More info on jtop can be found here: https://github.com/rbonghi/jetson_stats
+
+##To get simpletracker running on the nano do the following:
 
 1. Flash an SD card with Jetpack 4.6
 2. Boot your nano using the SD card and complete the system setup
-3. Launch the terminal application
+3. Launch the terminal
 4. Update your package lists by typing: ```sudo apt update```
 5. Upgrade your install by typing: ```sudo apt upgrade``` **NOTE:** This will take a while to run the first time as it needs to download and install all the system updates
    1.Select "Yes" to the docker restart prompt 
 6. Clone the git repository using the following command: ```git clone https://github.com/Sky360-Repository/simpletracker.git```
 7. Change directory to simpletracker/docker: ```cd ./simpletracker/docker```
 8. Run the run file: ```./run.sh``` - **NOTE:** This will take a while to run the first time as it needs to download all the required container layers
-9. Once you have a running command prompt type: ```cd /home/simpletracker```
-10. Run the nano run file: ```./nano-run.sh```
-    1. If you want output the results to a log file run the file using: ```./nano-run.sh >> timings_nano_20211204.txt```
+9. Once you have a running command prompt type: ```cd /home/simpletracker/scripts```
+10. Run the jetson run file: ```./jetson-run.sh```
+    1. If you want output the results to a log file run the file using: ```./nano-jetson.sh >> timings_nano_cuda_perf_test.txt``` where ***timings_nano_cuda_perf_test.txt*** is the name of the file containing the output.   
 11. When you are done type: ```exit``` to exit the container
 12. Then type: ```exit``` again to exit the terminal
 13. You can now use the file explorer to find your timings file which will be located in the simpletracker directory in your home folder
 14. What I usually do is launch the browser and go to your email e.g. GMail
     1. Create a new email and attach the timings file to the email, it will then save that email as draft, you can now access it from anywhere. Alternatively upload to something like Google Drive or One Drive.
 
-###Changing settings
+##Changing settings
 
 The run.sh file located in the docker folder will remove the settings.toml file and replace it using the settings.mike.toml file. 
 If you want setting changes to persist change both the settings.toml as well as the settings.mike.toml files. 
--- uap_tracker/dense_optical_flow_cuda.py
+++ uap_tracker/dense_optical_flow_cuda.py
@@ -10,28 +10,13 @@ class DenseOpticalFlowCuda():
         self.width = width
         self.height = height
 
-        # create hsv output for optical flow
-        # hsv = np.zeros((self.height, self.width, 3), np.float32)
-        # set saturation to 1
-        # hsv[..., 1] = 1.0
-
-        # self.gpu_hsv = cv2.cuda_GpuMat()
-        # self.gpu_hsv.upload(hsv)
-
-    #    def process_frame(self, frame):
-    #        grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #        self.process_grey_frame(grey_frame)
-
     def _resize_cuda(self, gpu_frame):
         return cv2.cuda.resize(gpu_frame, (self.width, self.height))
 
     def process_grey_frame(self, gpu_frame):
         gpu_frame = self._resize_cuda(gpu_frame)
 
-        # print(f'frame size {gpu_frame.size()}')
-
         gpu_bgr_frame = cv2.cuda_GpuMat()
-
         if self.previous_gpu_frame is None:
             # First time round, save frame and return blank image
             self.previous_gpu_frame = gpu_frame
@@ -46,27 +31,16 @@ class DenseOpticalFlowCuda():
             self.gpu_v_channel = cv2.cuda_GpuMat(gpu_frame.size(), cv2.CV_32FC1)
 
             # set saturation to 1
-            # print(f'np.ones_like {np.ones_like(self.previous_gpu_frame, np.float32)}')
-            # self.gpu_s_channel.upload(np.ones_like(self.previous_gpu_frame, np.float32))
             self.gpu_s_channel.upload(np.ones_like(gpu_frame.download(), np.float32))
 
             gpu_bgr_frame.upload(np.zeros((self.height, self.width), np.uint8))
 
             return gpu_bgr_frame
 
-        # convert to gray
-        # gpu_current = cv2.cuda.cvtColor(gpu_frame, cv2.COLOR_BGR2GRAY)
-
         # create optical flow instance
         gpu_flow = cv2.cuda_FarnebackOpticalFlow.create(5, 0.5, False, 15, 3, 5, 1.2, 0)
 
-        # gpu_flow = cv2.cuda.calcOpticalFlowFarneback(
-        #    self.previous_gpu_frame, gpu_current, None, 0.5, 5, 15, 3, 5, 1.2, 0,
-        # )
-
-        # print(f'previous_gpu_frame size {self.previous_gpu_frame.size()}')
-        # print(f'gpu_frame size {gpu_frame.size()}')
-
+        # calculate optical flow
         gpu_flow = cv2.cuda_FarnebackOpticalFlow.calc(gpu_flow, self.previous_gpu_frame, gpu_frame, None)
 
         gpu_flow_x = cv2.cuda_GpuMat(gpu_flow.size(), cv2.CV_32FC1)
@@ -75,20 +49,6 @@ class DenseOpticalFlowCuda():
 
         # convert from cartesian to polar coordinates to get magnitude and angle
         gpu_magnitude, gpu_angle = cv2.cuda.cartToPolar(gpu_flow_x, gpu_flow_y, angleInDegrees=True)
-
-        # convert from cartesian to polar coordinates to get magnitude and angle
-        # gpu_magnitude, gpu_angle = cv2.cuda.cartToPolar(
-        #    gpu_flow[..., 0], gpu_flow[..., 1], angleInDegrees=True,
-        # )
-
-        # set hue according to the angle of optical flow
-        ###self.gpu_hsv[..., 0] = gpu_angle * ((1 / 360.0) * (180 / 255.0))
-
-        # set value according to the normalized magnitude of optical flow
-        ###self.gpu_hsv[..., 2] = gpu_magnitude
-        # cv2.normalize(
-        #    magnitude, None, 0.0, 1.0, cv2.NORM_MINMAX, -1,
-        # )
 
         # set value to normalized magnitude from 0 to 1
         self.gpu_v_channel = cv2.cuda.normalize(gpu_magnitude, 0.0, 1.0, cv2.NORM_MINMAX, -1)
@@ -100,16 +60,10 @@ class DenseOpticalFlowCuda():
         # set hue according to the angle of optical flow
         self.gpu_h_channel.upload(angle)
 
-        # print(f'h channel size {self.gpu_h_channel.size()}')
-        # print(f's channel size {self.gpu_s_channel.size()}')
-        # print(f'v channel size {self.gpu_v_channel.size()}')
-        # print(f'hsv frame size {self.gpu_hsv_frame.size()}')
-
         # merge h,s,v channels
         cv2.cuda.merge([self.gpu_h_channel, self.gpu_s_channel, self.gpu_v_channel], self.gpu_hsv_frame)
 
         # multiply each pixel value to 255
-        # hsv_8u = np.uint8(self.hsv * 255.0)
         self.gpu_hsv_frame.convertTo(cv2.CV_8U, 255.0, self.gpu_hsv_8u_frame, 0.0)
 
         # convert hsv to bgr
