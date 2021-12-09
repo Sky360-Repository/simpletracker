485dd9d56116324c000dd862c71a697ccfb98a18
Michael G <michael@mwgtechnologies.co.uk>
2021-12-09 14:47:51 +0000

    Adding mog2 sensitivity varthresholds

--- uap_tracker/background_subtractor_factory.py
+++ uap_tracker/background_subtractor_factory.py
@@ -17,7 +17,7 @@ class BackgroundSubtractorFactory():
             # history = background_subtractor.getHistory()
             # print(f'samples:{samples}, dist2Threshold:{dist_2_threshold}, history, {history}')
 
-            background_subtractor.setHistory(1)  # large gets many detections
+            #background_subtractor.setHistory(1)  # large gets many detections
 
             # setkNNSamples() --> Sets the k in the kNN. How many nearest neighbours need to match
             #  -  1 doesn't detect small object
@@ -38,23 +38,29 @@ class BackgroundSubtractorFactory():
 
         if type == 'MOG2':
             background_subtractor = cv2.createBackgroundSubtractorMOG2()
-            background_subtractor.setHistory(1)  # large gets many detections
-            #if sensitivity == 1:  # Detects small, medium and large objects
-            #elif sensitivity == 2:  # Detects medium and large objects
-            #elif sensitivity == 3:  # Detects large objects
-            #else:
-            #    raise Exception(f"Unknown sensitivity option ({sensitivity}). 1, 2 and 3 is supported not {sensitivity}.")
+            #background_subtractor.setHistory(1)  # large gets many detections
+            if sensitivity == 1:  # Detects small, medium and large objects
+                background_subtractor.setVarThreshold(15)
+            elif sensitivity == 2:  # Detects medium and large objects
+                background_subtractor.setVarThreshold(25)
+            elif sensitivity == 3:  # Detects large objects
+                background_subtractor.setVarThreshold(50)
+            else:
+                raise Exception(f"Unknown sensitivity option ({sensitivity}). 1, 2 and 3 is supported not {sensitivity}.")
 
         if type == 'MOG2_CUDA':
-            background_subtractor = cv2.cuda.createBackgroundSubtractorMOG2()
-            background_subtractor.setHistory(1)  # large gets many detections
-            #if sensitivity == 1:  # Detects small, medium and large objects
-            #elif sensitivity == 2:  # Detects medium and large objects
-            #elif sensitivity == 3:  # Detects large objects
-            #else:
-            #    raise Exception(f"Unknown sensitivity option ({sensitivity}). 1, 2 and 3 is supported not {sensitivity}.")
+            if sensitivity == 1:  # Detects small, medium and large objects
+                background_subtractor = cv2.cuda.createBackgroundSubtractorMOG2(varThreshold=500) #, detectShadows=True)
+            elif sensitivity == 2:  # Detects medium and large objects
+                background_subtractor = cv2.cuda.createBackgroundSubtractorMOG2(varThreshold=1000) #, detectShadows=True)
+            elif sensitivity == 3:  # Detects large objects
+                background_subtractor = cv2.cuda.createBackgroundSubtractorMOG2(varThreshold=1500) #, detectShadows=True)
+            else:
+                raise Exception(f"Unknown sensitivity option ({sensitivity}). 1, 2 and 3 is supported not {sensitivity}.")
 
         if background_subtractor is None:
             raise Exception(f"Unknown background subtractor type ({type}).")
+        else:
+            background_subtractor.setHistory(1)  # large gets many detections
 
         return background_subtractor
