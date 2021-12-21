d4e10b60b72c12af44999eebe8cc29a7ded56921
Michael G <michael@mwgtechnologies.co.uk>
2021-12-21 13:34:37 +0000

    Formatting code to be more readable

--- uap_tracker/camera_stream_controller.py
+++ uap_tracker/camera_stream_controller.py
@@ -32,7 +32,9 @@ class CameraStreamController():
         frame_count = 0
         fps = 0
         with FrameProcessor.CPU() as processor:
+
             while True:
+
                 timer = cv2.getTickCount()
                 success, frame = self.camera.read()
                 if success:
@@ -40,12 +42,14 @@ class CameraStreamController():
                     # Calculate Frames per second (FPS)
                     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
                     frame_count += 1
+
                 if datetime.datetime.now() >= iteration_period:
                     # print(f"Iteration complete break")
                     if not self.video_tracker.is_tracking:
                         # print(f"Breaking loop")
                         self.video_tracker.finalise()
                         break
+
                 if cv2.waitKey(1) == 27:  # Escape
                     print(
                         f"Escape keypress detected, exit even if we are tracking: {self.video_tracker.is_tracking}")
--- uap_tracker/camera_stream_controller_cuda.py
+++ uap_tracker/camera_stream_controller_cuda.py
@@ -11,7 +11,9 @@ class CameraStreamControllerCuda(CameraStreamController):
         frame_count = 0
         fps = 0
         with FrameProcessor.GPU() as processor:
+
             while True:
+
                 timer = cv2.getTickCount()
                 success, frame = self.camera.read()
                 if success:
@@ -19,12 +21,14 @@ class CameraStreamControllerCuda(CameraStreamController):
                     # Calculate Frames per second (FPS)
                     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
                     frame_count += 1
+
                 if datetime.datetime.now() >= iteration_period:
                     # print(f"Iteration complete break")
                     if not self.video_tracker.is_tracking:
                         # print(f"Breaking loop")
                         self.video_tracker.finalise()
                         break
+
                 if cv2.waitKey(1) == 27:  # Escape
                     print(
                         f"Escape keypress detected, exit even if we are tracking: {self.video_tracker.is_tracking}")
