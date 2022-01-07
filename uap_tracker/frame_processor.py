66f8beff4c1c8c22c0d807bece86c866fe86898a
Michael G <michael@mwgtechnologies.co.uk>
2022-01-07 10:01:27 +0000

    Adding the skeleton for additional frame processor

--- uap_tracker/frame_processor.py
+++ uap_tracker/frame_processor.py
@@ -295,50 +295,146 @@ class GpuFrameProcessor(FrameProcessor):
         dof_frame = gpu_dof_frame.download()
         video_tracker.add_image(video_tracker.FRAME_TYPE_OPTICAL_FLOW, dof_frame)
 
-class VpiFrameProcessor(FrameProcessor):
+class NanoFrameProcessor(FrameProcessor):
 
     def __init__(self, dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius):
         super().__init__(dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius)
 
     def __enter__(self):
-        #print('VPI.__enter__')
+        #print('Nano.__enter__')
         return self
 
     def __exit__(self, type, value, traceback):
         pass
-        #print('VPI.__exit__')
+        #print('Nano.__exit__')
 
     def resize(self, gpu_frame, w, h, stream):
-        # Overload this for a VPI specific implementation
-        #print('VPI.resize_frame')
+        # Overload this for a Nano specific implementation
+        #print('Nano.resize_frame')
         pass
 
     def reduce_noise(self, gpu_frame, blur_radius, stream):
-        # Overload this for a VPI specific implementation
-        #print('VPI.noise_reduction')
+        # Overload this for a Nano specific implementation
+        #print('Nano.noise_reduction')
         pass
 
     def convert_to_grey(self, gpu_frame, stream):
-        # Overload this for a VPI specific implementation
-        #print('VPI.convert_to_grey')
+        # Overload this for a Nano specific implementation
+        #print('Nano.convert_to_grey')
         pass
 
     def keypoints_from_bg_subtraction(self, gpu_frame_grey, stream):
-        # Overload this for a VPI specific implementation
-        # print('VPI.keypoints_from_bg_subtraction')
+        # Overload this for a Nano specific implementation
+        # print('Nano.keypoints_from_bg_subtraction')
         pass
 
     def process_optical_flow(self, gpu_frame_grey, frame_w, frame_h, stream):
-        # Overload this for a VPI specific implementation
-        #print('VPI.process_optical_flow')
+        # Overload this for a Nano specific implementation
+        #print('Nano.process_optical_flow')
         pass
 
     def process_frame(self, video_tracker, frame, frame_count, fps, stream):
-        # Overload this for a VPI specific implementation
-        # print('VPI.process_frame')
+        # Overload this for a Nano specific implementation
+        # print('Nano.process_frame')
         pass
 
     def perform_optical_flow_task(self, video_tracker, frame_count, gpu_frame_grey, frame_w, frame_h, stream):
-        # Overload this for a VPI specific implementation
-        # print('VPI.perform_optical_flow_task')
+        # Overload this for a Nano specific implementation
+        # print('Nano.perform_optical_flow_task')
+        pass
+
+class XavierFrameProcessor(FrameProcessor):
+
+    def __init__(self, dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius):
+        super().__init__(dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius)
+
+    def __enter__(self):
+        #print('Xavier.__enter__')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
+        #print('Xavier.__exit__')
+
+    def resize(self, gpu_frame, w, h, stream):
+        # Overload this for a Xavier specific implementation
+        #print('Xavier.resize_frame')
+        pass
+
+    def reduce_noise(self, gpu_frame, blur_radius, stream):
+        # Overload this for a Xavier specific implementation
+        #print('Xavier.noise_reduction')
+        pass
+
+    def convert_to_grey(self, gpu_frame, stream):
+        # Overload this for a Xavier specific implementation
+        #print('Xavier.convert_to_grey')
+        pass
+
+    def keypoints_from_bg_subtraction(self, gpu_frame_grey, stream):
+        # Overload this for a Xavier specific implementation
+        # print('Xavier.keypoints_from_bg_subtraction')
+        pass
+
+    def process_optical_flow(self, gpu_frame_grey, frame_w, frame_h, stream):
+        # Overload this for a Xavier specific implementation
+        #print('Xavier.process_optical_flow')
+        pass
+
+    def process_frame(self, video_tracker, frame, frame_count, fps, stream):
+        # Overload this for a Xavier specific implementation
+        # print('Xavier.process_frame')
+        pass
+
+    def perform_optical_flow_task(self, video_tracker, frame_count, gpu_frame_grey, frame_w, frame_h, stream):
+        # Overload this for a Xavier specific implementation
+        # print('Xavier.perform_optical_flow_task')
+        pass
+
+class OrinFrameProcessor(FrameProcessor):
+
+    def __init__(self, dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius):
+        super().__init__(dense_optical_flow, background_subtractor, resize_frame, resize_dim, noise_reduction, mask_pct, detection_mode, detection_sensitivity, blur_radius)
+
+    def __enter__(self):
+        #print('Orin.__enter__')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
+        #print('Orin.__exit__')
+
+    def resize(self, gpu_frame, w, h, stream):
+        # Overload this for a Orin specific implementation
+        #print('Orin.resize_frame')
+        pass
+
+    def reduce_noise(self, gpu_frame, blur_radius, stream):
+        # Overload this for a Orin specific implementation
+        #print('Orin.noise_reduction')
+        pass
+
+    def convert_to_grey(self, gpu_frame, stream):
+        # Overload this for a Orin specific implementation
+        #print('Orin.convert_to_grey')
+        pass
+
+    def keypoints_from_bg_subtraction(self, gpu_frame_grey, stream):
+        # Overload this for a Orin specific implementation
+        # print('Orin.keypoints_from_bg_subtraction')
+        pass
+
+    def process_optical_flow(self, gpu_frame_grey, frame_w, frame_h, stream):
+        # Overload this for a Orin specific implementation
+        #print('Orin.process_optical_flow')
+        pass
+
+    def process_frame(self, video_tracker, frame, frame_count, fps, stream):
+        # Overload this for a Orin specific implementation
+        # print('Orin.process_frame')
+        pass
+
+    def perform_optical_flow_task(self, video_tracker, frame_count, gpu_frame_grey, frame_w, frame_h, stream):
+        # Overload this for a Orin specific implementation
+        # print('Orin.perform_optical_flow_task')
         pass
